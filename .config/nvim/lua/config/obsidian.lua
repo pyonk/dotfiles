f4243be537a45920f10cc9d52dcac4f52e40e616
pyonk <yukiyoshiokap@gmail.com>
2024-10-27 21:56:07 +0900

    update

--- .config/nvim/lua/config/obsidian.lua
+++ .config/nvim/lua/config/obsidian.lua
@@ -2,7 +2,30 @@ require("obsidian").setup({
 	workspaces = {
 		{
 			name = "personal",
-			path = "~/Documents/Obsidian Vault/"
+			path = "~/Documents/Obsidian Vault/",
+		},
+	},
+	mappings = {
+		-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+		["fo"] = {
+		  action = function()
+		    return require("obsidian").util.gf_passthrough()
+		  end,
+		  opts = { noremap = false, expr = true, buffer = true },
+		},
+		-- Toggle check-boxes.
+		["<leader>ch"] = {
+			action = function()
+				return require("obsidian").util.toggle_checkbox()
+			end,
+			opts = { buffer = true },
+		},
+		-- Smart action depending on context, either follow link or toggle checkbox.
+		["<cr>"] = {
+			action = function()
+				return require("obsidian").util.smart_action()
+			end,
+			opts = { buffer = true, expr = true },
 		},
 	},
 })
