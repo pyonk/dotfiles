ed4d2d87b5857ae22b73ca987ff61d9fa9c8259e
pyonk <yukiyoshiokap@gmail.com>
2024-12-29 08:04:21 +0900

    make background transparent

--- .config/nvim/lua/config/tokyonight.lua
+++ .config/nvim/lua/config/tokyonight.lua
@@ -1,2 +1,7 @@
-vim.g.tokyonight_sidebars = { "qf", "fern", "terminal", "packer", "lspsagaoutline" }
-vim.cmd('colorscheme tokyonight')
+local tokyonight = require('tokyonight')
+
+tokyonight.setup {
+  transparent = true,
+}
+-- vim.g.tokyonight_sidebars = { "qf", "fern", "terminal", "packer", "lspsagaoutline" }
+vim.cmd('colorscheme tokyonight-moon')
